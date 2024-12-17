62fc56bb0cd19a6b8508349081a5c0fbc394db2c
ctf16 <ctf16@pitt.edu>
2024-12-17 18:14:51 -0500

    getSchedule() grabs schedule array based on sch and alg

--- Assets/Values.cs
+++ Assets/Values.cs
@@ -4,22 +4,23 @@ public class Values : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     float jitter_value;
-    int schedule_chosen;
+    int[] schedule_chosen;
 
-    public void init(float val, int sch){
+    public void init(float val, int sch, int algo){
         Debug.Log("init");
         Debug.Log(val);
         Debug.Log(sch);
+        Debug.Log(algo);
 
         jitter_value = val;
-        schedule_chosen = sch;
+        schedule_chosen = GetComponent<ScheduleManager>().GetSchedule(sch, algo);
     }
 
     public float getJitter(){
         return jitter_value;
     }
 
-    public int getSchedule(){
+    public int[] getSchedule(){
         return schedule_chosen;
     }
 }
